cb7c5a91263482df9340cda223827c45158dce89
cgranda <Cesar.Granda@baruch.cuny.edu>
2023-06-22 16:29:10 -0400

    testing error 3

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,59 +1,63 @@
-import { React, useState, useEffect } from 'react';
-import parse  from 'react-html-parser';
+// import educationContent from "../data/education";
+const coverName = params.get('covername');
 
-const url = new URL(window.location.href);
-const params = new URLSearchParams(url.search);
-const paramCName = params.get('covername');
-console.log(paramCName);
+const Cover = () => (
+<div class="section row">
+    <div class="section-text col-right row address">
+        <div class="col light">
+            <p>Phone: (347) 495-4107 I Email: info@cgranda.com I Address: 48 Liberty Avenue, New Jersey</p>
+        </div>
+    </div>
 
-function Cover() {
-    const [cover, getCover] = useState([]);
-    useEffect(()=> {
-        coverMDB()
-    }, [])
+    <h2 class="coverletter">Cover Letter</h2>
 
-    const coverMDB = async () => {
-        const response = await fetch('https://resume-mern.onrender.com/fetch-cover');
-
-        getCover(await response.json())
-    }
-    console.log(cover);   
-    return (
-     
-        <>
-        <div class="section row">
-            <div class="section-text col-right row address">
-                <div class="col light">
-                    <p>Phone: (347) 495-4107 I Email: info@cgranda.com I Address: 48 Liberty Avenue, New Jersey</p>
-                </div>
+    <div class="section-text col-right row">
+            <div class="col">
             </div>
+            <div class="col">        
+            <h4>Dear Executive Director of Web Services,</h4>
+            <p>I am writing to express my strong interest in the position of 
+            Design and Development Engineer at Susan G. Komen. With my extensive experience in 
+            client-side and server-side development, database design, and cloud modules, I am confident
+            in my ability to contribute to your team and drive the success of your projects.</p>
 
-            <h2 class="coverletter">Cover Letter</h2>
+            <p>Throughout my career, I have demonstrated a proven track record in designing and developing robust 
+            software solutions. I have successfully designed and implemented complex database schemas, 
+            ensuring efficient data storage and retrieval. My leadership skills have allowed me to provide 
+            guidance and direction for testing software, guaranteeing the delivery of high-quality, responsive, 
+            and efficient applications.</p>
 
-            <div class="section-text col-right row">
-                    <div class="col">
-                    </div>
-                    <div class="col">                            
-                        {cover.map((val) => (
-                        <>
-                                <div key={val._id}> 
-                                {paramCName === val.coverName  && (
-                                    <>
-                                    <h4>Dear {val.dear},</h4>
-                                      {parse(val.letter)}
-                                    </>
-                                    )
-                                }                                    
-                                </div>
-                        </>
-                        ))}               
-                    <p>Sincerely,</p>
-                    <h4>Cesar Granda</h4>
-                    </div>
+            <p>I am highly proficient in developing APIs and possess a deep understanding of 
+            troubleshooting and debugging techniques. My expertise extends to mentoring and coaching 
+            fellow developers, enabling them to enhance their skills and deliver exceptional results. 
+            I am committed to staying updated with the latest industry trends and have hands-on experience 
+            in using responsive web development principles, including CSS/JS frameworks such as Bootstrap 
+            and Foundation.</p>
+
+            <p>Moreover, I have a strong background in requirements analysis, utilizing techniques 
+            like user stories, use cases, and informal modeling. I have also built highly-scalable web 
+            applications and cloud-native applications, leveraging my in-depth knowledge of developing 
+            user interfaces that seamlessly communicate with APIs. With over 10 years of experience in 
+            middle-tier and back-end languages, including PHP and C# .NET, I possess the technical 
+            expertise to meet the demands of this role. Additionally, I have 8+ years of experience 
+            in front-end languages and libraries, such as HTML/CSS, JavaScript, XML, and jQuery. 
+            My proficiency in data modeling further enhances my ability to contribute 
+            effectively to your projects.</p>
+
+            <p>My passion for continuous learning and growth 
+            drives me to seek innovative solutions and stay at the forefront of technological advancements.</p>
+
+            <p>I am excited about the opportunity to join the talented team at Susan G. Komen
+            and contribute to its ongoing success. Thank you for considering my application. 
+            I would welcome the chance to discuss further how my skills and experience align 
+            with your needs. Please find my attached resume for more information.</p>
+
+            <p>Please feel free to contact me at your convenience.Thank you for your time and consideration.</p>
+
+            <p>Sincerely,</p>
+            <h4>Cesar Granda</h4>
             </div>
-        </div>  
-
-        </>
-    )
-}
+    </div>
+</div>	
+);
 export default Cover;
