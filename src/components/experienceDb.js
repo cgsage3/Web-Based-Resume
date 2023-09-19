037ef6e9e526f1a168f72bd104a49de8ce5826cc
cgranda <Cesar.Granda@baruch.cuny.edu>
2023-09-19 09:31:03 -0400

    inverted experience

--- src/components/experienceDb.js
+++ src/components/experienceDb.js
@@ -30,10 +30,10 @@ function ExperienceDb() {
 
 function compare( a, b ) {
   if ( a._id < b._id ){
-    return -1;
+    return 1;
   }
   if ( a._id > b._id ){
-    return 1;
+    return -1;
   }
   return 0;
 }
